735e796c02ff6da7cacf0345c299f402c8f89828
Dylan wu <dylanwoo@Dylans-MacBook-Pro.local>
2023-06-07 11:40:52 +0800

    feat: init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [input, setInput] = useState("");
+  const operators = [
+    "AC",
+    "DEL",
+    "÷",
+    "",
+    1,
+    2,
+    3,
+    "*",
+    4,
+    5,
+    6,
+    "+",
+    7,
+    8,
+    9,
+    "-",
+    ".",
+    0,
+    "=",
+  ];
+
+  const handleClick = (val: string | number) => {
+    if (val === "=") {
+    }
+    setInput(() => `${input}` + `${val}`);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputArea">{input}</div>
+      <div className="container">
+        {operators.map((o) => (
+          <div className="area" key={o} onClick={() => handleClick(o)}>
+            {o}
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
-
-export default App;
